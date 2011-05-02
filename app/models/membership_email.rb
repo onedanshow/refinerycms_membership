2f755b9e36d1bc94d2301d6c1fbe670c1999f6c2
Hamish Evans <hamish@estate3d.com>
2011-05-03 11:55:05 +1200

    Translate the correct fields

--- app/models/membership_email.rb
+++ app/models/membership_email.rb
@@ -1,7 +1,7 @@
 class MembershipEmail < ActiveRecord::Base
-  translates :subject, :body
+  translates :subject, :text
   
   validates_uniqueness_of :title
   
-  validates_presence_of :subject, :body
+  validates_presence_of :subject, :text
 end
